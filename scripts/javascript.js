1d96ce0470d4fbe737b7b549385cf3371970002a
Shadowedcross <shadowedcrossrl@gmail.com>
2024-07-31 20:21:25 +0100

    Add function that returns user input if valid
    Created a function that takes input from the user, and checks if it's
    one of the valid options ("rock", "paper", or "scissors"). If the input
    is valid, the value is returned and checked using console.log(),
    otherwise an alert dialog is displayed instructing the user to enter a
    valid option, then the user is re-prompted to enter input.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,4 +2,21 @@ function getComputerChoice () {
     let randomNumber = Math.random();
     return randomNumber <= 0.33 ? "rock" : randomNumber <= 0.66 ? "paper" : "scissors"
 }
+
 console.log(getComputerChoice())
+
+function getHumanChoice () {
+    let input
+    let validInput = false
+    while (!validInput) {
+        input = prompt("Your choice: ").toLowerCase();
+        if (input !== "rock" && input !== "paper" && input !== "scissors") {
+            alert("Please choose a correct type ('rock', 'paper' or 'scissors'), input is case-insensitive");
+        } else {
+            validInput = true;
+        }
+    }
+    return input
+}
+
+console.log(getHumanChoice())
