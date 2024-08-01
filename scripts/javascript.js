2ebe5d9556373c7bb3e3a746385f700c7b22e3e3
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-01 12:09:10 +0100

    Add functions to allow for playing a single round
    Added playRound and calcVictoryState functions. The latter determines
    whether the player won, lost, or drew. The former calls the latter, and
    based on the return value, increments either the user's, or the
    computer's, score, and outputs an appropriate console line.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,8 +3,6 @@ function getComputerChoice () {
     return randomNumber <= 0.33 ? "rock" : randomNumber <= 0.66 ? "paper" : "scissors"
 }
 
-console.log(getComputerChoice())
-
 function getUserChoice () {
     let input
     let validInput = false
@@ -19,7 +17,35 @@ function getUserChoice () {
     return input
 }
 
-console.log(getUserChoice())
-
 let userScore = 0
 let computerScore = 0
+
+function playRound (userChoice, computerChoice) {
+    let victoryState = calcVictoryState(userChoice, computerChoice)
+    if (victoryState === "won") {
+        userScore++
+        console.log(`You won! ${userChoice} beats ${computerChoice}.`)
+    } else if (victoryState === "lost") {
+        computerScore++
+        console.log(`You lost! ${userChoice} loses against ${computerChoice}.`)
+    } else {
+        console.log(`You drew! You both used ${userChoice}.`)
+    }
+}
+
+function calcVictoryState(userChoice, computerChoice) {
+    let victoryState
+    if (userChoice === "rock") {
+        victoryState = computerChoice === "rock" ? "drew" : computerChoice === "paper" ? "lost" : "won"
+    } else if (userChoice === "paper") {
+        victoryState = computerChoice === "rock" ? "won" : computerChoice === "paper" ? "drew" : "lost"
+    } else if (userChoice === "scissors") {
+        victoryState = computerChoice === "rock" ? "lost" : computerChoice === "paper" ? "won" : "drew"
+    }
+    return victoryState
+}
+
+let userChoice = getUserChoice()
+let computerChoice = getComputerChoice()
+
+playRound(userChoice, computerChoice)
