6435d7c699c6ba43fc93356be4cd68326b9c7b14
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-01 13:14:06 +0100

    Add function to allow playing a set of rounds
    I added a function (playGame) that allows you to play a set of rounds.
    This is currently set to always play a set of 5.
    I moved the round logic, including playRound and calcVictoryState into
    it.
    The playRound function now tells the user the current score after
    every round.
    After all rounds have been played, the playGame function tells the user
    if they won, lost, or drew.
    I also added failsafes to allow the user to cancel the prompt, as
    otherwise it would break the script.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -7,7 +7,15 @@ function getUserChoice () {
     let input
     let validInput = false
     while (!validInput) {
-        input = prompt("Your choice: ").toLowerCase();
+        let rawInput = prompt("Your choice: ")
+
+        if (rawInput === null || rawInput === '') {
+            input = null
+            break;
+        } else {
+            input = rawInput.toLowerCase()
+        }
+
         if (input !== "rock" && input !== "paper" && input !== "scissors") {
             alert("Please choose a correct type ('rock', 'paper' or 'scissors'), input is case-insensitive");
         } else {
@@ -17,35 +25,64 @@ function getUserChoice () {
     return input
 }
 
-let userScore = 0
-let computerScore = 0
+const DEFAULT_ROUNDS_TO_PLAY = 5
 
-function playRound (userChoice, computerChoice) {
-    let victoryState = calcVictoryState(userChoice, computerChoice)
-    if (victoryState === "won") {
-        userScore++
-        console.log(`You won! ${userChoice} beats ${computerChoice}.`)
-    } else if (victoryState === "lost") {
-        computerScore++
-        console.log(`You lost! ${userChoice} loses against ${computerChoice}.`)
-    } else {
-        console.log(`You drew! You both used ${userChoice}.`)
+function playGame (roundsToPlay = DEFAULT_ROUNDS_TO_PLAY) {
+
+    function playRound (userChoice, computerChoice, currentRound) {
+        let victoryState = calcVictoryState(userChoice, computerChoice)
+        if (victoryState === "won") {
+            userScore++
+            console.log(`Round ${currentRound}: You won! ${userChoice} beats ${computerChoice}.`)
+        } else if (victoryState === "lost") {
+            computerScore++
+            console.log(`Round ${currentRound}: You lost! ${userChoice} loses against ${computerChoice}.`)
+        } else {
+            console.log(`Round ${currentRound}: You drew! You both used ${userChoice}.`)
+        }
+        if (victoryState === "drew") {
+            console.log(`Current Scores: User: ${userScore} Computer: ${computerScore}`)
+        } else {
+            console.log(`New Scores: User: ${userScore} Computer: ${computerScore}`)
+        }
+    }
+
+    function calcVictoryState(userChoice, computerChoice) {
+        let victoryState
+        if (userChoice === "rock") {
+            victoryState = computerChoice === "rock" ? "drew" : computerChoice === "paper" ? "lost" : "won"
+        } else if (userChoice === "paper") {
+            victoryState = computerChoice === "rock" ? "won" : computerChoice === "paper" ? "drew" : "lost"
+        } else if (userChoice === "scissors") {
+            victoryState = computerChoice === "rock" ? "lost" : computerChoice === "paper" ? "won" : "drew"
+        }
+        return victoryState
+    }
+
+    let userScore = 0
+    let computerScore = 0
+    let userCanceled = false
+
+    for (let i = 0; i < roundsToPlay; i++) {
+        let userChoice = getUserChoice()
+        if (userChoice === null) {
+            console.log("User Canceled")
+            userCanceled = true
+            break;
+        }
+        let computerChoice = getComputerChoice()
+        playRound(userChoice, computerChoice, i + 1)
+    }
+
+    if (!userCanceled) {
+        if ( userScore > computerScore) {
+            console.log(`Congratulations, you won!\nEnd scores: User: ${userScore} Computer: ${computerScore}`)
+        } else if (userScore < computerScore) {
+            console.log(`You lost! Better luck next time.\nEnd Scores: User: ${userScore} Computer: ${computerScore}`)
+        } else {
+            console.log(`You drew! Maybe a rematch is in order? End Scores: User: ${userScore} Computer: ${computerScore}`)
+        }
     }
 }
 
-function calcVictoryState(userChoice, computerChoice) {
-    let victoryState
-    if (userChoice === "rock") {
-        victoryState = computerChoice === "rock" ? "drew" : computerChoice === "paper" ? "lost" : "won"
-    } else if (userChoice === "paper") {
-        victoryState = computerChoice === "rock" ? "won" : computerChoice === "paper" ? "drew" : "lost"
-    } else if (userChoice === "scissors") {
-        victoryState = computerChoice === "rock" ? "lost" : computerChoice === "paper" ? "won" : "drew"
-    }
-    return victoryState
-}
-
-let userChoice = getUserChoice()
-let computerChoice = getComputerChoice()
-
-playRound(userChoice, computerChoice)
+playGame()
