0bc70d2a81e981d9cbaebf309455bd52cbc84094
Shadowedcross <shadowedcrossrl@gmail.com>
2024-08-03 10:54:54 +0100

    Add UI to allow playing without console
    Added a paragraph to show the score, a paragraph to show the round
    results, and a div that holds three buttons for user input. Once the
    score of either the user, or the computer, reaches 5, the winner is
    announced, and the buttons are disabled. Can make it replayable in the
    future - without refreshing, but the assignment didn't specify that it
    had to be replayable.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,88 +1,79 @@
+let userChoice;
+let userScore = 0;
+let computerScore = 0;
+
+let results = document.querySelector(".results");
+let score = document.querySelector(".score");
+let choiceButtonsContainer = document.querySelector(".choice-buttons-container")
+
+const POINTS_FOR_VICTORY = 5
+let gameEnded = false
+
 function getComputerChoice () {
     let randomNumber = Math.random();
     return randomNumber <= 0.33 ? "rock" : randomNumber <= 0.66 ? "paper" : "scissors"
 }
 
-function getUserChoice () {
-    let input
-    let validInput = false
-    while (!validInput) {
-        let rawInput = prompt("Your choice: ")
-
-        if (rawInput === null || rawInput === '') {
-            input = null
-            break;
-        } else {
-            input = rawInput.toLowerCase()
-        }
-
-        if (input !== "rock" && input !== "paper" && input !== "scissors") {
-            alert("Please choose a correct type ('rock', 'paper' or 'scissors'), input is case-insensitive");
-        } else {
-            validInput = true;
-        }
+function playRound (userChoice) {
+    let computerChoice = getComputerChoice()
+    let roundVictoryState = calcRoundVictoryState(userChoice, computerChoice)
+    if (roundVictoryState === "won") {
+        userScore++
+        updateResults(`You won! ${userChoice} beats ${computerChoice}.`)
+    } else if (roundVictoryState === "lost") {
+        computerScore++
+        updateResults(`You lost! ${userChoice} loses against ${computerChoice}.`)
+    } else {
+        updateResults(`You drew! You both used ${userChoice}.`)
     }
-    return input
-}
-
-const DEFAULT_ROUNDS_TO_PLAY = 5
-
-function playGame (roundsToPlay = DEFAULT_ROUNDS_TO_PLAY) {
-
-    function playRound (userChoice, computerChoice, currentRound) {
-        let victoryState = calcVictoryState(userChoice, computerChoice)
-        if (victoryState === "won") {
-            userScore++
-            console.log(`Round ${currentRound}: You won! ${userChoice} beats ${computerChoice}.`)
-        } else if (victoryState === "lost") {
-            computerScore++
-            console.log(`Round ${currentRound}: You lost! ${userChoice} loses against ${computerChoice}.`)
-        } else {
-            console.log(`Round ${currentRound}: You drew! You both used ${userChoice}.`)
-        }
-        if (victoryState === "drew") {
-            console.log(`Current Scores: User: ${userScore} Computer: ${computerScore}`)
-        } else {
-            console.log(`New Scores: User: ${userScore} Computer: ${computerScore}`)
-        }
-    }
-
-    function calcVictoryState(userChoice, computerChoice) {
-        let victoryState
-        if (userChoice === "rock") {
-            victoryState = computerChoice === "rock" ? "drew" : computerChoice === "paper" ? "lost" : "won"
-        } else if (userChoice === "paper") {
-            victoryState = computerChoice === "rock" ? "won" : computerChoice === "paper" ? "drew" : "lost"
-        } else if (userChoice === "scissors") {
-            victoryState = computerChoice === "rock" ? "lost" : computerChoice === "paper" ? "won" : "drew"
-        }
-        return victoryState
-    }
-
-    let userScore = 0
-    let computerScore = 0
-    let userCanceled = false
-
-    for (let i = 0; i < roundsToPlay; i++) {
-        let userChoice = getUserChoice()
-        if (userChoice === null) {
-            console.log("User Canceled")
-            userCanceled = true
-            break;
-        }
-        let computerChoice = getComputerChoice()
-        playRound(userChoice, computerChoice, i + 1)
-    }
-
-    if (!userCanceled) {
-        if ( userScore > computerScore) {
-            console.log(`Congratulations, you won!\nEnd scores: User: ${userScore} Computer: ${computerScore}`)
-        } else if (userScore < computerScore) {
-            console.log(`You lost! Better luck next time.\nEnd Scores: User: ${userScore} Computer: ${computerScore}`)
-        } else {
-            console.log(`You drew! Maybe a rematch is in order? End Scores: User: ${userScore} Computer: ${computerScore}`)
-        }
+    if (roundVictoryState !== "drew") {
+        updateScore()
     }
 }
 
-playGame()
+function calcRoundVictoryState(userChoice, computerChoice) {
+    let roundVictoryState
+    if (userChoice === "rock") {
+        roundVictoryState = computerChoice === "rock" ? "drew" : computerChoice === "paper" ? "lost" : "won"
+    } else if (userChoice === "paper") {
+        roundVictoryState = computerChoice === "rock" ? "won" : computerChoice === "paper" ? "drew" : "lost"
+    } else if (userChoice === "scissors") {
+        roundVictoryState = computerChoice === "rock" ? "lost" : computerChoice === "paper" ? "won" : "drew"
+    }
+    return roundVictoryState
+}
+
+function updateResults (value) {
+    results.textContent = value;
+}
+
+function updateScore () {
+    score.textContent = `You ${userScore} : ${computerScore} Computer`
+    if (userScore === POINTS_FOR_VICTORY) {
+        setGameWinner("user");
+    } else if (computerScore === POINTS_FOR_VICTORY) {
+        setGameWinner("computer");
+    }
+}
+
+function setGameWinner(value) {
+    gameEnded = true
+    disableButtons()
+    if (value === "user") {
+        updateResults("Congratulations! You were the first to reach 5 points!")
+    } else {
+        updateResults("Unlucky! The computer reached 5 points before you.")
+    }
+}
+
+function disableButtons() {
+    for (const button of choiceButtonsContainer.children) {
+        button.setAttribute("disabled", "disabled")
+    }
+}
+
+choiceButtonsContainer.addEventListener("click", (event) => {
+    let target = event.target;
+    userChoice = target.className;
+    playRound(userChoice, getComputerChoice())
+})
